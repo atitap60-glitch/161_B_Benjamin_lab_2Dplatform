d84a63c9cb5c68bd64d679f16c15392aba4fdbb4
atitap60-glitch <atit.ap60@gmail.com>
2025-10-28 11:36:38 +0700

    jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,58 +1,81 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class PlayerController : Character
 {
+    public float maxSpeed = 10f;
+    public float jumpForce = 300f;
 
-	public float maxSpeed = 10f;
-	bool facingRight = true;
+    bool facingRight = true;
+    Rigidbody2D r2d;
+    Animator anim;
 
+    [Header("Ground Check")]
+    public bool grounded = false;
+    public Transform groundCheck;
+    public float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
 
-	Rigidbody2D r2d;
-	Animator anim;
+    void Start()
+    {
+        r2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 
-	public bool grounded = false;
-	public Transform groundCheck;
-	float groundRadius = 0.2f;
-	public LayerMask whatIsGround;
-	public float jumpForce = 700.0f;
+        if (groundCheck == null)
+            Debug.LogError("groundCheck not assigned!");
+    }
 
-	// Use this for initialization
-	void Start () {
-		r2d = GetComponent<Rigidbody2D>	();
-		anim = GetComponent<Animator>();
-	}
-	
-	void Update(){
-		if( grounded && Input.GetKeyDown(KeyCode.Space) ){
-			anim.SetBool("Ground", false);
-			r2d.AddForce(new Vector2(0, jumpForce));
-		}
-	}
+    void Update()
+    {
+        // Jump input
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
 
-	void FixedUpdate () {
+    void FixedUpdate()
+    {
+        // Ground check
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", grounded);
+        anim.SetFloat("vSpeed", r2d.linearVelocity.y);
 
-		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
-		anim.SetBool("Ground", grounded);
+        // Horizontal movement
+        float move = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", Mathf.Abs(move));
 
-		anim.SetFloat("vSpeed", r2d.linearVelocity.y);
+        // Better: Use velocity for smooth control, but don't touch Y during jump
+        Vector2 velocity = r2d.linearVelocity;
+        velocity.x = move * maxSpeed;
+        r2d.linearVelocity = velocity;
 
-		float move = Input.GetAxis("Horizontal");
+        // Flip sprite
+        if (move > 0 && !facingRight) Flip();
+        else if (move < 0 && facingRight) Flip();
+    }
 
-		anim.SetFloat("Speed", Mathf.Abs(move));
+    void Jump()
+    {
+        anim.SetBool("Ground", false);
+        r2d.AddForce(new Vector2(0, jumpForce));
+        grounded = false; // Prevent double jump
+    }
 
-		r2d.linearVelocity = new Vector2(move*maxSpeed, r2d.linearVelocity.y);
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
 
-		if(move > 0 && !facingRight)
-			Flip();
-		else if( move < 0 && facingRight )
-			Flip();
-	}
-
-	void Flip(){
-		facingRight = !facingRight;
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
-	}
+    // Optional: Visualize ground check in editor
+    void OnDrawGizmosSelected()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = grounded ? Color.green : Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, groundRadius);
+        }
+    }
 }
